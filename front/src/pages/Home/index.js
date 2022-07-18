d3853a2bc089abccde6b6092dfc0fb1de8092f95
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-18 16:29:38 -0300

    feat: update random images

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 const Home = () => {
 
     const [data, setData] = useState([]);
+    const [image, setImage] = useState([]);
 
     const getData = async () => {
         const response = await fetch('http://localhost:3001/');
@@ -10,11 +11,23 @@ const Home = () => {
         setData(dados);
     };
 
+    const endPoints = [
+        "https://randomuser.me/api/?results=1",
+        "https://randomuser.me/api/?results=1"
+    ]
+
+    const getUserImageProfile = async () => {
+        const response = await fetch("https://randomuser.me/api/?results=1")
+        const images = await response.json();
+        setImage(images.results)
+    }
+
     useEffect(() => {
         getData()
+        getUserImageProfile()
     }, []);
 
-    return(
+    return (
         <>
             <main>
                 <div className="cards">
@@ -22,12 +35,23 @@ const Home = () => {
                         {
                             data && (
                                 data.map((item) => {
-                                   return( <>
+                                    return (<>
                                         <h1>{item.name}</h1>
+                                        {
+                                            image.length > 0 &&(
+                                                image.map((item) => {
+                                                    return (
+                                                        <>
+                                                            <img src={item.picture.large} alt="image profile" />
+                                                        </>)
+                                                })
+                                            )
+                                        }
                                     </>)
                                 })
                             )
                         }
+
                     </div>
                 </div>
             </main>
